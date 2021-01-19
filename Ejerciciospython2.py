4c4268990e041bb618e8f2d14ba003eec1f5d39d
Pedro <pedrodavidpalacios1@gmail.com>
2021-01-19 20:54:56 +0100

    Ejercicios+

---
+++ Ejerciciospython2.py
@@ -0,0 +1,28 @@
+
+#Ejercicio 2
+x = ["Matematicas","Fisica","Quimica","Historia","Lengua"]
+print(x)
+
+#Ejercicio 3
+print("Yo estudio", x)
+
+#Ejercicio 4
+
+x = x + ["Matematicas"]
+
+print(x)
+y = set(x)
+print(y)
+
+#Ejercicio 5
+
+numeros = [1,2,3]
+cadenas = ["hola","mundo"]
+numeros.append(cadenas)
+print(numeros)
+
+
+#Ejercicio 7
+x = [1,2,3,4,5]
+[y**2 for y in x]
+print(x) 
